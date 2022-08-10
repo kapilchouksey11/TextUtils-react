bb2dd9c1a62c7ca9b9ccdd9f7a3a19a975bc532f
Kapil Chouksey <76171817+kapilchouksey11@users.noreply.github.com>
2022-08-10 12:43:27 +0530

    TextUtils
    TextUtils_first_commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import {// copied from documentationn
+//   BrowserRouter as Router,
+//   Route,
+//   Routes
+// } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light');//Whether dark mode is enabled or not.
+  const[alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled.", "success");
+      document.title = 'TextUtils - Dark Mode';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      // document.body.style.color = 'black';
+      showAlert("Light mode has been enabled.", "success")
+      document.title = 'TextUtils - Light Mode';
+    }
+  }
+
+  // const redMode = () => {
+  //   setMode('red');
+  //   document.body.style.backgroundColor = '#b55e67ab';
+    
+  //   // document.navbar.style.backgroundColor = '#bb2d3b';
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* <Router> */}
+    <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    {/* copied from documentation, now Switch is not available in REACT */}
+      {/* <Routes>
+        <Route exact path="/" element={ */}
+          <TextForm showAlert={showAlert} heading="Enter The text to analyze below" mode={mode}/>
+         {/* } />
+         <Route exact path="/about" element={<About />} />
+       </Routes> */}
+    
     </div>
+    {/* </Router> */}
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,79 @@
+import React,{useState} from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpClick = () =>{
+        // console.log("Upper case was clicked " + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to upperCase!","success")
+      }
+      
+      const handleLowClick = () =>{
+        // console.log("Lower case was clicked " + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to lowerCase!","success")
+    }
+
+    const handleClearText = () =>{
+        // console.log("Lower case was clicked " + text);
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text is now clear!","success")
+    }
+
+    const handleOnChange = (event) =>{
+        // console.log("On change");
+        setText(event.target.value); //Now we can type inside textarea.
+    }
+
+
+    const handleCopy = () => {
+      var text = document.getElementById("myBox");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+      props.showAlert("Copied to Clipbord!","success")
+    }
+
+
+    const handleExtraSpaces = () =>{
+      let newText = text.split(/[ ]+/);
+      setText(newText.join(" "));
+      props.showAlert("Removed ExtraSpaces!","success")
+    }
+
+
+  const [text, setText] = useState('');  //default string ''
+//   text = "New text";//Wrong way to change the state.
+//   setText("New Text"); //Correct way to change the text
+  return (
+    <>
+    <div className="container" style={{color : props.mode === 'dark'?'white':'black'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor : props.mode === 'dark'?'gray':'white',color : props.mode === 'dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+        </div>
+        
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to upper case</button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to lower case</button>
+        <button className="btn btn-primary mx-2 my-3" onClick={handleClearText}>Clear text</button>
+        <button className="btn btn-primary mx-2 my-3" onClick={handleCopy}>Copy text</button>
+        <button className="btn btn-primary mx-2 my-3" onClick={handleExtraSpaces}>Clear Extra Spaces</button>
+        
+    </div>
+    <div className="container my-3" style={{color : props.mode === 'dark'?'white':'black'}}>
+      <h1>Your text summary</h1>
+    <p><b>{text.split(" ").length}</b> Words and <b>{text.length}</b> characters</p>
+    {/* array = text.split(" ") will give an array */}
+    {/* array.length give the size of array. OR number of words.*/}
+    
+    <p>{0.008 * text.split(" ").length} Minutes read</p>
+    <h3>Preview</h3>
+    <p>{text.length>0?text:"Enter something above in text box to preview it here."}</p>
+    </div>
+    </>
+  )
+}
+
+
